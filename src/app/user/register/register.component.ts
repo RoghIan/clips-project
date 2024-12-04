feb3e14b5e19f25b830ee3fbdd54a95e8546bb2b
Rogh Ian <roghiansawangen111@gmail.com>
2024-12-04 13:53:13 +0800

    added custom validators

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { InputComponent } from '../../shared/input/input.component'
 import { AlertComponent } from '../../shared/alert/alert.component'
 import { AuthService } from '../../services/auth.service'
+import { EmailTaken, Match } from './validators'
 
 @Component({
 	selector: 'app-register',
@@ -20,37 +21,45 @@ import { AuthService } from '../../services/auth.service'
 export class RegisterComponent {
 	fb = inject(FormBuilder)
 	auth = inject(AuthService)
+	emailTaken = inject(EmailTaken)
 	showAlert = signal(false)
 	alertMsg = signal('Please wait your account is being created.')
 	alertColor = signal('blue')
 	inSubmission = signal(false)
 
-	form = this.fb.nonNullable.group({
-		name: ['', [Validators.required, Validators.minLength(3)]],
-		email: ['', [Validators.required, Validators.email]],
-		age: [
-			18,
-			[Validators.required, Validators.min(18), Validators.max(120)]
-		],
-		password: [
-			'',
-			[
-				Validators.required,
-				Validators.pattern(
-					/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
-				)
+	form = this.fb.nonNullable.group(
+		{
+			name: ['', [Validators.required, Validators.minLength(3)]],
+			email: [
+				'',
+				[Validators.required, Validators.email],
+				[this.emailTaken.validate]
+			],
+			age: [
+				18,
+				[Validators.required, Validators.min(18), Validators.max(120)]
+			],
+			password: [
+				'',
+				[
+					Validators.required,
+					Validators.pattern(
+						/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+					)
+				]
+			],
+			confirmPassword: ['', [Validators.required]],
+			phoneNumber: [
+				'',
+				[
+					Validators.required,
+					Validators.minLength(13),
+					Validators.maxLength(13)
+				]
 			]
-		],
-		confirmPassword: ['', [Validators.required]],
-		phoneNumber: [
-			'',
-			[
-				Validators.required,
-				Validators.minLength(13),
-				Validators.maxLength(13)
-			]
-		]
-	})
+		},
+		{ validators: [Match('password', 'confirmPassword')] }
+	)
 
 	async register() {
 		this.showAlert.set(true)
